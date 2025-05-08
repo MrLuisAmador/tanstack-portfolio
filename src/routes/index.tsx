45d625543b67d48d9be85602e4e605275df408ad
Luis Amador <mrluisamador@gmail.com>
2025-05-08 00:30:46 -0400

    updating this tanstack project to the newest version

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,24 @@
-import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { seo } from '~/utils/seo'
 
 export const Route = createFileRoute('/')({
   component: Home,
+  head: () => ({
+    meta: [
+          ...seo({
+            title:
+              'Fucking home page',
+            description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+          }),
+        ],
+  }),
 })
 
 function Home() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3>Welcome Home!!!</h3>
+      <p>This is static text</p>
     </div>
   )
 }
